af14af1b183097492b7abfb36aad5718bee7c3e0
BukkapatnamSunil <bukkapatnamsunil@gmail.com>
2023-11-10 10:42:12 +0530

    Initial starter project

---
+++ src/test/java/com/tyss/happyhome/HappyHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyss.happyhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
