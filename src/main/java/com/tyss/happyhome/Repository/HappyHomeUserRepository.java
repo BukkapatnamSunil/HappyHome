435069bb620e8b7b99a35cb033d5a3d613af724b
Pravalika-10 <dullapj1016@gmail.com>
2023-11-11 16:57:44 +0530

    updated dao

--- src/main/java/com/tyss/happyhome/Repository/HappyHomeUserRepository.java
+++ src/main/java/com/tyss/happyhome/Repository/HappyHomeUserRepository.java
@@ -1,9 +1,12 @@
 package com.tyss.happyhome.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
 import com.tyss.happyhome.entity.Users;
 
+@Component
 public interface HappyHomeUserRepository extends JpaRepository<Users, Integer>{
-
+	
+	Users findByEmail(String email);
 }
